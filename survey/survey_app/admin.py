de6c8eeac70de04f8ce5030e683580d8e09447bc
helen <helenuzhegova@gmail.com>
2022-01-15 00:24:41 +0700

    added admin model

---
+++ survey/survey_app/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Survey, Question, Choice
+
+
+class SurveyAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'start_date')
+    list_filter = ('title',)
+    list_display_links = ('title',)
+    search_fields = ('title', 'description')
+
+
+admin.site.register(Survey, SurveyAdmin)
+admin.site.register(Question)
+admin.site.register(Choice)
+
+
