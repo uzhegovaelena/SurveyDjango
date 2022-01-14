1a62cbc6cc102b9da31db235bfd9c4965428a301
helen <helenuzhegova@gmail.com>
2022-01-15 00:44:51 +0700

    added serializers

---
+++ survey/survey_app/serializer.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Survey, Question
+
+
+class SurveySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Survey
+        fields = '__all__'
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class meta:
+        model = Question
+        fields = '__all__'
